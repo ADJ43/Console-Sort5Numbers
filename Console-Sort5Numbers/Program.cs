93fb46e08b0df7bb5f3a67845e0c29f6517b7e67
ADJ43 <adjse43@gmail.com>
2023-12-10 15:53:26 -0500

    Initial commit

---
+++ Console-Sort5Numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Sort5Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+            Console.WriteLine("Enter 5 unique numbers:");
+
+            while (numbers.Count < 5)
+            {
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    if (numbers.Contains(number))
+                    {
+                        Console.WriteLine("You have already entered this number. Please enter a different number.");
+                    }
+                    else
+                    {
+                        numbers.Add(number);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            var sortedNumbers = new List<int>(numbers);
+            sortedNumbers.Sort();
+
+            Console.WriteLine("Sorted numbers:");
+            foreach (int num in sortedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
